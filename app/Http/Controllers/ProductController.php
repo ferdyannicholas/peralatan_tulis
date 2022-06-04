51a9a85a3bda8855bf4521026972d68564ebdda9
mimmaseliana <mimmaseli29@gmail.com>
2022-06-04 13:33:28 +0700

    anither

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-//© 2020 Copyright: Tahu Coding
+
 use File;
 use App\Product;
 use App\HistoryProduct;
@@ -163,5 +163,4 @@ class ProductController extends Controller
         
     }
 
-    //© 2020 Copyright: Tahu Coding
 }
--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,4 @@ Route::group(['middleware' => ['auth']], function () {
 
 
 
+
